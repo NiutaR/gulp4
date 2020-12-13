7db907fa1788e559ab3daf8204f829e201facea3
NiutaR <69516148+NiutaR@users.noreply.github.com>
2020-12-13 21:05:58 +0200

    optimazed

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
-const imagemin = require('gulp-imagemin');
-
-let project_folder = "dist";
+let project_folder = require("path").basename(__dirname);
 let source_folder = "#src";
+let fs = require('fs');
 
 let path = {
     build: {
@@ -42,10 +41,17 @@ let {
     rename = require("gulp-rename"),
     uglify = require("gulp-uglify-es").default,
     imagemin = require("gulp-imagemin"),
-    webp = require('gulp-webp');
+    webp = require('gulp-webp'),
+    webphtml = require('gulp-webp-html'),
+    webpcss = require("gulp-webpcss"),
+    svgSprite = require('gulp-svg-sprite'),
+    ttf2woff = require('gulp-ttf2woff'),
+    ttf2woff2 = require('gulp-ttf2woff2'),
+    fonter = require('gulp-fonter');
 
 
-function browserSync(params) {
+
+function browserSync() {
     browsersync.init({
         server: {
             baseDir: "./" + project_folder + "/"
@@ -58,6 +64,7 @@ function browserSync(params) {
 function html() {
     return src(path.src.html)
         .pipe(fileinclude())
+        .pipe(webphtml())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
@@ -78,6 +85,7 @@ function css() {
                 cascade: true
             })
         )
+        .pipe(webpcss())
         .pipe(dest(path.build.css))
         .pipe(clean_css())
         .pipe(
@@ -107,25 +115,79 @@ function js() {
 
 function images() {
     return src(path.src.img)
-    .pipe(
-        webp({
-            quality: 70
-        })
-    )
-    .pipe
-    .pipe(dest(path.build.img))
-    .pipe(src(path.src.img))
-    .pipe(
-        imagemin({
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            interlaced: true,
-            optimizationLevel: 3
-        })
-    )
+        .pipe(
+            webp({
+                quality: 70
+            })
+        )
+        .pipe(dest(path.build.img))
+        .pipe(src(path.src.img))
+        .pipe(
+            imagemin({
+                progressive: true,
+                svgoPlugins: [{
+                    removeViewBox: false
+                }],
+                interlaced: true,
+                optimizationLevel: 3
+            })
+        )
         .pipe(dest(path.build.img))
         .pipe(browsersync.stream())
 }
+
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts));
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts));
+};
+
+gulp.task('otf2ttf', function () {
+    return src([source_folder + '/fonts/*.otf'])
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(dest(source_folder + '/fonts'));
+})
+
+gulp.task('svgSprite', function () {
+    return gulp.src([source_folder + '/iconsprite/*.svg'])
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: "../icons/icons.svg",
+                    example: true
+                }
+            },
+        }))
+        .pipe(dest(path.build.img))
+})
+
+function fontsStyle(params) {
+
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb() {}
 
 function watchFiles() {
     gulp.watch([path.watch.html], html);
@@ -138,9 +200,11 @@ function clean() {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
